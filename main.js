1584fbec20aae3613cdb9ebb98993e2277c8f656
Lucas Santos <lhs.santoss@gmail.com>
2020-10-27 18:26:15 -0300

    🎨  Improve code

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('Hello World')
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('./screens/index.html')
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
